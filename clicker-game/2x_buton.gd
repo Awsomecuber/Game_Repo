3ef8c2944c93e548280b6f9f05b914b448162309
Awsomecuber <awsomecuber13@gmail.com>
2024-12-17 23:10:51 +0800

    From export

--- clicker-game/2x_buton.gd
+++ clicker-game/2x_buton.gd
@@ -6,6 +6,6 @@ func _on_pressed() -> void:
 	if main.coin >= 5:
 		main.coin -= 5
 		main.click += 2
-	if main.coin < 4:
+	if main.coin < 5:
 		print("Not Enough Coins")
 		print("Balance: %s" % main.coin)
