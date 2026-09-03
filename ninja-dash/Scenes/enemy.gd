86ce7652e5d51bfe711fdd589f54f30e8fb6cbaf
Awsomecuber <awsomecuber13@gmail.com>
2026-09-03 08:15:22 +0800

    Fixed Enemy AI Animation

--- ninja-dash/Scenes/enemy.gd
+++ ninja-dash/Scenes/enemy.gd
@@ -6,6 +6,8 @@ const JUMP_VELOCITY = -400.0
 @onready var progress_bar: ProgressBar = $ProgressBar
 var player: CharacterBody2D
 var is_chaseable: bool= false
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	player = Global.playerBody
@@ -18,19 +20,33 @@ func _process(_delta: float) -> void:
 		queue_free()
 
 func _physics_process(delta: float) -> void:
+	var dir_x: float = 0
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	if is_chaseable and is_instance_valid(player):
-		chase()
+		dir_x = chase()
+		animation_player.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation_player.play("Idle")
+		
+	animate(dir_x)
 
 	move_and_slide()
+	
+func animate(dir_x: float):
+	if  dir_x > 0:
+		animated_sprite.flip_h = true
+		animated_sprite.offset.x = 5
+	elif dir_x < 0:
+		animated_sprite.flip_h = false
+		animated_sprite.offset.x = 0
 
-func chase():
+func chase() -> float:
 	var dir_x = sign(player.position.x - position.x)
 	velocity.x = dir_x * SPEED
+	return dir_x
 
 func _on_hurtbox_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Hitbox"):
