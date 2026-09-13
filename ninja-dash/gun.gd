ab85f8a08d8c63bed2eb19c4cde32a05e74bfdc4
Awsomecuber <awsomecuber13@gmail.com>
2026-09-13 23:53:12 +0800

    Added mouse and Controller input

--- ninja-dash/gun.gd
+++ ninja-dash/gun.gd
@@ -1,17 +1,16 @@
 extends Node2D
 
 const BULLET = preload("res://Scenes/bullet.tscn")
+@export var crosshair: Node2D
 
 func _process(_delta: float) -> void:
-	look_at(get_global_mouse_position())
+	look_at(crosshair.global_position)
 	
-	rotation_degrees = wrap(rotation_degrees, 0, 360)
-	
-	if rotation_degrees > 90 and rotation_degrees < 270:
-		scale.y = -1
-	else:
-		scale.y = 1
-		
+	#if rotation_degrees > 90 and rotation_degrees < 270:
+		#scale.y = -1
+	#else:
+		#scale.y = 1
+
 	if Input.is_action_just_pressed("shoot"):
 		var bullet = BULLET.instantiate()
 		get_tree().root.add_child(bullet)
--- ninja-dash/sprite_2d.gd
+++ ninja-dash/sprite_2d.gd
@@ -1,11 +1,23 @@
 extends Sprite2D
 
+const SPEED: float = 250.0
+const STICK_DEADZONE: float = 0.2
 
-# Called when the node enters the scene tree for the first time.
+enum InputMode { MOUSE, CONTROLLER }
+var current_mode: InputMode = InputMode.MOUSE
+
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		current_mode = InputMode.MOUSE
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	global_position = lerp(global_position, get_global_mouse_position(), 18*delta)
+	var stick_input = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
+
+	if stick_input.length() > STICK_DEADZONE:
+		current_mode = InputMode.CONTROLLER
+		global_position += stick_input * SPEED * delta
+	elif current_mode == InputMode.MOUSE:
+		global_position = get_global_mouse_position()
